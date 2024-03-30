ef6f7a79cbef6affcdbeeeefc94879836e4d7217
carlosbolanos96 <carlosbolanos309@gmail.com>
2024-03-30 15:46:39 -0500

    1

---
+++ src/com/codeinova/carbonfootptintcalculator/Main.java
@@ -0,0 +1,8 @@
+package com.codeinova.carbonfootptintcalculator;
+import static com.codeinova.functions.vehicles.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car(3);
+    }
+}
---
+++ src/com/codeinova/functions/vehicles.java
@@ -0,0 +1,9 @@
+package com.codeinova.functions;
+
+public class vehicles {
+    public static int Car(int type){
+
+        int CarCarbon =0;
+        return CarCarbon;
+    }
+}
